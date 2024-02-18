07d90882533bb5b25943bc33a5023dae64fa870e
meepleky <58671806+meepleky@users.noreply.github.com>
2024-02-17 20:54:02 -0600

    largeExport.js
    Changing the export code to download a text file instead of outputting the data in a prompt. The prompt seemed to only give me 38 items, which likely varies depending on the character length of the items.

--- export.js
+++ export.js
@@ -1,1 +1,27 @@
-prompt("Copy this:", localStorage.getItem("infinite-craft-data"));
+// prompt("Copy this:", localStorage.getItem("infinite-craft-data"));
+// Add option to export to file (The above seems to only export about 38 items)
+// I don't know how to code in JS, just a heads up. I'm using ChatGPT here
+
+var data = localStorage.getItem("infinite-craft-data");
+var filename = "exported_data.txt";
+var filetype = "text/plain";
+    // Create a Blob object with the data
+    var blob = new Blob([data], { type: filetype });
+
+    // Create a temporary URL for the Blob object
+    var url = URL.createObjectURL(blob);
+
+    // Create a link element
+    var link = document.createElement('a');
+
+    // Set the href attribute of the link to the temporary URL
+    link.href = url;
+
+    // Set the download attribute to the desired filename
+    link.download = filename;
+
+    // Programmatically click the link to trigger the download
+    link.click();
+
+    // Clean up by revoking the temporary URL
+    URL.revokeObjectURL(url);
